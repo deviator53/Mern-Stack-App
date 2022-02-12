e09d90ba7388122c3fe76723725862a9e11e6b20
David Bradford <davidbradforduko@yahoo.com>
2022-02-12 22:07:31 +0100

    first work

---
+++ rest-api/controllers/goalController.js
@@ -0,0 +1,33 @@
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler(async (req, res) => {
+    res.json({message: 'Get Goals'});
+
+})
+
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text){
+        res.status(400);
+        throw new Error('Please add a text');
+    }
+    res.status(200).json({message: 'Set Goals'});
+
+})
+
+const updateGoal = asyncHandler(async (req, res) => {
+    res.json({message: `Update Goal ${req.params.id}`});
+
+})
+
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.json({message: `Delete Goal ${req.params.id}`});
+
+})
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+} 
